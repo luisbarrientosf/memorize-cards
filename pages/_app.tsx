f838ed22384718c8b31bf77b55a52dd5a68bb0e5
Luis Barrientos <luis.barrientos.faj@gmail.com>
2024-01-18 19:29:10 -0300

    refactor: AppProps added on _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import '../app/globals.css';
+import { AppProps } from 'next/app';
 import { Oswald } from 'next/font/google';
 
 
 const inter = Oswald({ subsets: ['latin'] })
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps } : AppProps) {
   return (
     <main className={inter.className}>
       <Component {...pageProps} />
