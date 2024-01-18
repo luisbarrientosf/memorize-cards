50c0c103f58073a969287cd52d921016d0aa193a
Luis Barrientos <luis.barrientos.faj@gmail.com>
2024-01-18 01:50:54 -0300

    chore: add jest + Button test

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type {Config} from 'jest';
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config: Config = {
+  transform: {
+    ".(ts|tsx)": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js"
+  ],
+  clearMocks: true,
+  testEnvironment: "jsdom",
+};
+
+export default createJestConfig(config)
---
+++ tests/presentation/components/Button.spec.tsx
@@ -0,0 +1,30 @@
+import '@testing-library/jest-dom'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { Button } from "../../../src/presentation/components/Button/Button"
+
+describe('Button', () => {
+  it('renders correctly', () => {
+    render(
+      <Button
+        title='test'
+        onClick={() => {}}
+      />
+    );
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+  });
+
+  it('fire onClick function when button clicked', () => {
+    const spyFn = jest.fn()
+    render(
+      <Button
+        title='test'
+        onClick={spyFn}
+      />
+    );
+    const button = screen.getByText('test');
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+    expect(spyFn).toHaveBeenCalledTimes(1);
+  });
+});
